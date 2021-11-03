b6a020950319fa52c74da990d8d9ea25f3b2e6ea
h2o-creator <hadysalah1455@gmail.com>
2021-11-03 22:34:42 +0200

    Send internal API request using fetch and get data

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -4,5 +4,42 @@ export default function handleSubmit(event) {
     event.preventDefault()
     const text = document.getElementById('text').value
     if (!validateData(text)) return alert('Invalid input text!')
-    console.log(text)
+    fetch('/sentiment', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ txt: text })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        const { agreement, confidence, irony, score_tag, sentence_list } = data
+        const resultElement = document.getElementById('result')
+        const docFrag = document.createDocumentFragment()
+
+        const agreementElement = document.createElement('p')
+        agreementElement.textContent = `AGREEMENT: ${agreement}`
+        docFrag.appendChild(agreementElement)
+
+        const confidenceElement = document.createElement('p')
+        confidenceElement.textContent = `CONFIDENCE: ${confidence}`
+        docFrag.appendChild(confidenceElement)
+
+        const ironyElement = document.createElement('p')
+        ironyElement.textContent = `IRONY: ${irony}`
+        docFrag.appendChild(ironyElement)
+
+        const scoreTagElement = document.createElement('p')
+        scoreTagElement.textContent = `SCORE TAG: ${score_tag}`
+        docFrag.appendChild(scoreTagElement)
+
+        sentence_list.map((sentence) => {
+            const sentenceElement = document.createElement('p')
+            sentenceElement.textContent = `SENTENCE: ${sentence.text} CONFIDENCE: ${sentence.confidence} SCORE: ${sentence.score_tag} BOP: ${sentence.bop}`
+            docFrag.appendChild(sentenceElement)
+        })
+    
+        resultElement.appendChild(docFrag)
+    })
 }
