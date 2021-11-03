46dbce50ea4620810b3f8c7e40e650eab97b7cad
h2o-creator <hadysalah1455@gmail.com>
2021-11-03 22:53:31 +0200

    Install and configure service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -30,7 +31,8 @@ module.exports = {
             template: './src/client/views/index.html',
             filename: './index.html'
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new WorkboxPlugin.GenerateSW(),
     ],
     optimization: {
         minimize: true,
